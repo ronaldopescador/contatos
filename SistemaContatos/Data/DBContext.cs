00795a33079c94572ed868a5945864893985cf3c
Ronaldo Pescador <ronaldopescador@gmail.com>
2025-03-27 21:54:59 -0300

    Inicial

---
+++ SistemaContatos/Data/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaContatos.Models;
+
+    public class DBContext : DbContext
+    {
+        public DBContext (DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaContatos.Models.Contato> Contato { get; set; } = default!;
+    }
---
+++ SistemaContatos/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DBContext") ?? throw new InvalidOperationException("Connection string 'DBContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
